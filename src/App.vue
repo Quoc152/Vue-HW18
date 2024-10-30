eb63e8932b95c001fe898ce621dc80b4b5768732
Quoc Le <quocle152@gmail.com>
2024-10-30 14:29:10 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import CustomButton from "./components/CustomButton.vue";
+import { UserIcon, ArrowRightIcon, MailIcon, SendIcon } from "lucide-vue-next";
+</script>
+
+<template>
+  <div
+    class="container h-screen py-8 flex flex-col gap-3 justify-center items-center"
+  >
+    <CustomButton>Demo Button</CustomButton>
+
+    <CustomButton
+      ><template #prefix-icon>
+        <UserIcon class="h-5 w-5" />
+      </template>
+      With Prefix Icon
+    </CustomButton>
+
+    <CustomButton>
+      With Suffix Icon
+      <template #suffix-icon>
+        <ArrowRightIcon class="h-5 w-5" />
+      </template>
+    </CustomButton>
+
+    <CustomButton>
+      <template #prefix-icon>
+        <MailIcon class="h-5 w-5" />
+      </template>
+      With Both Icons
+      <template #suffix-icon>
+        <SendIcon class="h-5 w-5" />
+      </template>
+    </CustomButton>
+
+    <CustomButton :isLoading="true"> Loading Button </CustomButton>
+  </div>
+</template>
---
+++ src/components/CustomButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <button
+    class="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 disabled:opacity-50 disabled:cursor-not-allowed"
+    :disabled="isLoading"
+  >
+    <slot name="prefix-icon"></slot>
+    <span class="mx-2">
+      <slot></slot>
+    </span>
+    <slot name="suffix-icon">
+      <Loader2Icon v-if="isLoading" class="animate-spin h-5 w-5" />
+    </slot>
+  </button>
+</template>
+
+<script setup>
+import { Loader2Icon } from "lucide-vue-next";
+
+defineProps({
+  isLoading: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
